12f9dd703f255384bdeedefb4e3fbce2fd03a5f8
murilonicemento <murilo_nascimento22@outlook.com>
2023-04-18 18:28:11 -0300

    Configuração das Rotas

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import globalStyles from "./styles/globalStyles";
 import { Home } from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import globalStyles from "./styles/globalStyles";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/techstack" element={<TechStack />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
