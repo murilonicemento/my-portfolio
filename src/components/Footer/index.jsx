a57efdabd93905489dd7b7511d74f735ab8ffb62
murilonicemento <murilo_nascimento22@outlook.com>
2023-04-19 18:19:31 -0300

    Finalização da Estilização do Footer no Layout Mobile

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,49 @@
+import { Link } from "react-router-dom";
+import { Footer } from "./styled";
+import github from "../../assets/images/github.svg";
+import linkedin from "../../assets/images/linkedin.svg";
+import whatsapp from "../../assets/images/whatsapp.svg";
+
+export function MyFooter() {
+  return (
+    <Footer>
+      <div>
+        <p className="logo">&lt;/MN&gt;</p>
+        <ul>
+          <li>
+            <a href="">+55 (31) 98114 - 1827</a>
+          </li>
+          <li>
+            <a href="">murilo_nascimento22@outlook.com</a>
+          </li>
+        </ul>
+        <div className="socialMedia">
+          <img src={github} alt="GitHub Profile" />
+          <img src={linkedin} alt="LinkedIn Profile" />
+          <img src={whatsapp} alt="WhatsApp Profile" />
+        </div>
+      </div>
+      <div id="divider"></div>
+      <div id="navigationBar">
+        <ul>
+          <li>
+            <Link to="/home">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">Sobre</Link>
+          </li>
+          <li>
+            <Link to="/techstack">Tech Stack</Link>
+          </li>
+          <li>
+            <Link to="/projects">Projetos</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contato</Link>
+          </li>
+        </ul>
+        <p>Made with (heart) by Murilo Nascimento</p>
+      </div>
+    </Footer>
+  );
+}
