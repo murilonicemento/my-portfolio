8f29d26d3debfc0367995df1deba0386cfc9d4be
murilonicemento <murilo_nascimento22@outlook.com>
2023-04-21 11:17:03 -0300

    Finalização da estilização da pagina not found

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,3 +1,19 @@
+import { Page404 } from "./styled";
+import notFound from "../../assets/images/notfound.svg";
+import face404 from "../../assets/images/face404.svg";
+import arrow from "../../assets/images/arrow.svg";
+
 export function NotFound() {
-  return <>Esse aqui é o Not Found</>;
+  return (
+    <Page404>
+      <h1>Oops!</h1>
+      <p>Você está perdido</p>
+      <img src={notFound} alt="" />
+      <img src={face404} alt="Face" />
+      <button>
+        <img src={arrow} alt="Arrow" />
+        Voltar para Home
+      </button>
+    </Page404>
+  );
 }
---
+++ src/pages/NotFound/styled.jsx
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+import * as colors from "../../config/colors";
+
+export const Page404 = styled.section`
+  display: grid;
+  place-items: center;
+  margin-top: 55px;
+
+  p {
+    margin-top: 8px;
+  }
+
+  img {
+    width: 90%;
+  }
+
+  img:nth-child(4) {
+    width: 40%;
+    position: absolute;
+    top: 245px;
+    left: 30px;
+  }
+
+  button {
+    background-color: ${colors.backgroundColor};
+    font-family: "Poppins", sans-serif;
+    color: ${colors.fontColor};
+    border: none;
+    outline: none;
+    margin-top: 64px;
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    border-bottom: 1px solid ${colors.fontColor};
+    cursor: pointer;
+
+    img {
+      width: 30px;
+      height: 30px;
+    }
+  }
+`;
