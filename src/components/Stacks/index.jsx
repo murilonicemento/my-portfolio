5b5b81f6f20749673d8e876d61f1411ee2824652
murilonicemento <murilo_nascimento22@outlook.com>
2023-10-04 17:21:45 -0300

    inclusão de novas imagens de tecnologias

--- src/components/Stacks/index.jsx
+++ src/components/Stacks/index.jsx
@@ -1,12 +1,13 @@
-import { Stacks } from "./styled";
-import html from "../../assets/images/html.svg";
 import css from "../../assets/images/css.svg";
-import sass from "../../assets/images/sass.svg";
-import javascript from "../../assets/images/javascript.svg";
 import git from "../../assets/images/git.svg";
+import html from "../../assets/images/html.svg";
+import javascript from "../../assets/images/javascript.svg";
+import php from "../../assets/images/php.svg";
 import react from "../../assets/images/react.svg";
-import typescript from "../../assets/images/typescript.svg";
+import sass from "../../assets/images/sass.svg";
 import sqlite from "../../assets/images/sqlite.svg";
+import typescript from "../../assets/images/typescript.svg";
+import { Stacks } from "./styled";
 
 export function MyStacks() {
   return (
@@ -15,10 +16,11 @@ export function MyStacks() {
       <img src={css} alt="CSS Icon" />
       <img src={sass} alt="SASS Icon" />
       <img src={javascript} alt="JavaScript Icon" />
-      <img src={git} alt="Git Icon" />
       <img src={react} alt="React Icon" />
       <img src={typescript} alt="React Icon" />
+      <img src={php} alt="React Icon" />
       <img src={sqlite} alt="SQLite Icon" />
+      <img src={git} alt="Git Icon" />
     </Stacks>
   );
 }
