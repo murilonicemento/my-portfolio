07631d6efec285ec872c8a143c71195a8ba02bcc
murilonicemento <murilo_nascimento22@outlook.com>
2024-06-13 15:01:50 -0300

    feat: add hamburguer menu and hover card to header

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,41 +1,20 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import { RouterLink } from "vue-router";
-
-const links = ref([
-  { id: 1, name: "Início", path: "/" },
-  { id: 2, name: "Sobre", path: "/about" },
-  { id: 3, name: "Projetos", path: "/projects" },
-  { id: 4, name: "Contato", path: "/contact" }
-]);
+import HamburguerMenu from "@/components/HamburguerMenu.vue";
+import HoverCard from "./icons/HoverCard.vue";
 </script>
 
 <template>
   <header>
-    MuriloDev
-    <nav>
-      <RouterLink :to="link.path" v-for="link in links" :key="link.id">{{ link.name }}</RouterLink>
-    </nav>
+    <HoverCard />
+    <HamburguerMenu />
   </header>
 </template>
 
-<style lang="css">
+<style scoped lang="css">
 header {
   display: flex;
   align-items: center;
   justify-content: space-between;
   padding: 16px;
-}
-#menu {
-  width: 24px;
-  display: flex;
-  flex-direction: column;
-  gap: 4px;
-  border: 1px solid blue;
-  cursor: pointer;
-}
-
-#menu div {
-  border: 1px solid red;
 }
 </style>
