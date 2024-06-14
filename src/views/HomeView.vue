f0020842bd6d8de0d3cc7a1a52f4348f0cf0939e
murilonicemento <murilo_nascimento22@outlook.com>
2024-06-13 23:22:38 -0300

    feat: define struct of sections

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,42 @@ import HeaderComponent from "@/components/HeaderComponent.vue";
 
 <template>
   <HeaderComponent />
-  <h1>Home View</h1>
+  <main>
+    <section>
+      <h3>Olá, meu nome é Murilo Nascimento</h3>
+      <h3>Sou um desenvolvedor fullsterco</h3>
+      <p>
+        Desenvolvedora de software, apaixonada pela arte do Web Design, dedicada a criar interfaces
+        visualmente cativantes. Meu foco no Front End traz vida aos designs, enquanto minha
+        expertise em UI/UX garante que cada interação seja não apenas bonita, mas também intuitiva.
+      </p>
+      <div><a href=""></a><a href=""></a><a href=""></a></div>
+
+      <img src="" alt="" />
+    </section>
+    <section>
+      <h3>Sobre mim</h3>
+      <img src="" alt="" />
+      <ul>
+        <li>Bio</li>
+        <li>Educação</li>
+        <li>Experiência</li>
+      </ul>
+    </section>
+    <section>
+      <h3>Tecnologias</h3>
+    </section>
+    <section>
+      <h3>Projetos</h3>
+    </section>
+    <section>
+      <h3>Contato</h3>
+    </section>
+  </main>
 </template>
 
-<style></style>
+<style scoped lang="css">
+section {
+  height: 100vh;
+}
+</style>
