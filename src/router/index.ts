762fe28b1581b6e92842ef0c2579e999dc242684
murilonicemento <murilo_nascimento22@outlook.com>
2024-06-28 17:46:10 -0300

    refactor: remove unecessary routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,21 +8,6 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView
-    },
-    {
-      path: "/about",
-      name: "about",
-      component: HomeView
-    },
-    {
-      path: "/skills",
-      name: "skills",
-      component: HomeView
-    },
-    {
-      path: "/contact",
-      name: "contact",
-      component: HomeView
     }
   ],
   scrollBehavior(to) {
