1a16279d1429c6420978509ced928995a482071b
murilonicemento <murilo_nascimento22@outlook.com>
2024-07-29 10:40:41 -0300

    feat: config tsparticle

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import HomeView from "@/views/HomeView.vue";
+import ParticleJS from "./components/ParticleJS.vue";
 </script>
 
 <template>
+  <ParticleJS />
   <HomeView />
 </template>
 
