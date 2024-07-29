31fc0b526e5aa98fda4d2a4b90d6139d965f8bc0
murilonicemento <murilo_nascimento22@outlook.com>
2024-07-29 10:41:33 -0300

    feat: config ts particle

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import "@/assets/main.css";
-
+import Particles from "@tsparticles/vue3";
+import { loadSlim } from "@tsparticles/slim";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-const app = createApp(App);
+const app = createApp(App).use(Particles, {
+  init: async (engine) => {
+    await loadSlim(engine);
+  }
+});
 
 app.use(router);
 
