231196d556ecf7094369053adaa8b56ca56acf6a
murilonicemento <murilo_nascimento22@outlook.com>
2024-09-05 16:34:14 -0300

    feat: add header component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import HeaderComponent from "./components/HeaderComponent.vue";
+</script>
 
 <template>
+  <HeaderComponent />
   <RouterView />
 </template>
 
